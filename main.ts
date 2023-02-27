85a312facad98146b838e7b891b05b6a92e9c76d
Rah-Jose-Israel <jossorio-pro@outlook.com>
2023-02-26 21:30:52 -0500

    rotation with a slapsh of happyness

--- main.ts
+++ main.ts
@@ -1,2 +1,13 @@
 console.log("taking Shape")
-console.log("baking Shape")
+
+
+const imageElement: HTMLImageElement | null = document.querySelector('img');
+
+
+if (imageElement) {
+    imageElement.addEventListener('click', function() {
+      imageElement.style.transform = 'perspective(600px) rotateY(45deg)';
+      imageElement.style.animation = 'rotate 2s linear infinite';
+    });
+  }
+   
